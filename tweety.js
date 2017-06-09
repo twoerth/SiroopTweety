6111e0b65e4a991a38111d26d7816a5b597257c0
twoerth <twoerth@gmail.com>
2017-06-09 17:37:29 +0200

    working version, removed urls

--- tweety.js
+++ tweety.js
@@ -21,26 +21,45 @@ function handleTweeetyClick( e ) {
 function handleSubmit( e ) {
     var content = $("#gmTextArea").val();
     var url = document.location.href;
-    console.log( { content: content, url: url } );    
+
+    var post = { content: content, url: url, ts: (new Date()).getTime() };
+    console.log( post );
+
+    $.ajax({
+        url: "http://127.0.0.1:8000/store",
+        type: 'POST',
+        crossDomain: true,
+        data: JSON.stringify( post ),
+        dataType: 'json',
+        contentType: "application/json",
+        success: function (data) {
+            console.log("Data: " + data);
+        }
+    });
 }
 
 function makeTweetyClickable() {
     // We could find the element with jQuery, but I came up with the path before deciding to include jQuery.
     var tweety = getElementByXpath('//html/body/header/div[@class="o-page__navigation-meta"]/div/nav/ul/img');
 
-    $("body").append (
-        '<div id="gmOverlayDialog" class="OverlayHidden">' +
-        '<textarea id="gmTextArea" rows="6" cols="50"">Type your comment here</textarea>'+
-        '<input id="gmSubmit" type="submit" value="Submit"></input>'+
-        '</div>'
-    );
+    if( tweety === null ) {
+        console.log("no tweety yet");
+        window.setTimeout( makeTweetyClickable, 200 );
+    } else {
+        $("body").append (
+            '<div id="gmOverlayDialog" class="OverlayHidden">' +
+            '<textarea id="gmTextArea" rows="6" cols="50"">Type your comment here</textarea>'+
+            '<input id="gmSubmit" type="submit" value="Submit"></input>'+
+            '</div>'
+        );
 
-    GM_addStyle("#gmOverlayDialog {height: 200px; width: 250px; background: #FFFFFF; position: fixed; right: 5px; top: 42px; border: 1px Solid #CCCCCC;  z-index: 5000;}");
-    GM_addStyle(".OverlayHidden {display: none;}");
+        GM_addStyle("#gmOverlayDialog {height: 200px; width: 250px; background: #FFFFFF; position: fixed; right: 5px; top: 42px; border: 1px Solid #CCCCCC;  z-index: 5000;}");
+        GM_addStyle(".OverlayHidden {display: none;}");
 
-    $("#gmSubmit").on("click", handleSubmit);
+        $("#gmSubmit").on("click", handleSubmit);
 
-    tweety.addEventListener("click", handleTweeetyClick);
+        tweety.addEventListener("click", handleTweeetyClick);
+    }
 }
 
 (function() {
@@ -49,5 +68,5 @@ function makeTweetyClickable() {
     console.log("Tampermonkey running");
 
     // Tweety is added with JS so we have to wait a bit to give it a chance to appear.
-    window.setTimeout( makeTweetyClickable, 1000 );
+    window.setTimeout( makeTweetyClickable, 200 );
 })();
